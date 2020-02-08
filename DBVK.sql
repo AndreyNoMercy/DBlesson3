e0fec96e8d377d400701922208be9e35f5521814
Andrey Borodin <andrey88borodin@gmail.com>
2020-02-08 07:09:40 +0500

    lesson 4

--- DBVK.sql
+++ DBVK.sql
@@ -75,3 +75,60 @@ CREATE TABLE media_types (
   media_type_id INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
   name VARCHAR(255) NOT NULL UNIQUE
 );
+
+#Доработка моих данных
+
+USE vk;
+
+UPDATE friendship SET requested_at = confirmed_at WHERE requested_at > confirmed_at;
+
+UPDATE media SET created_at = updated_at WHERE created_at > updated_at;
+UPDATE media SET `size` = 1 WHERE `size` = 0; 
+UPDATE media SET metadata = CONCAT('{"owner":"', (SELECT CONCAT(first_name, ' ', last_name) FROM users WHERE id = user_id),'"}');
+
+UPDATE profiles SET created_at = updated_at WHERE created_at > updated_at;
+UPDATE profiles SET created_at = birthdate WHERE birthdate > created_at;
+CREATE TEMPORARY TABLE sex (sex CHAR(1));
+INSERT INTO sex VALUES ('m'), ('f');
+SELECT * FROM sex;
+UPDATE profiles SET sex = (SELECT sex FROM sex ORDER BY RAND() LIMIT 1);
+UPDATE profiles SET sex = (SELECT sex FROM sex ORDER BY RAND() LIMIT 1);
+UPDATE profiles SET photo_id  = 1 + RAND() * 100; 
+
+DELETE FROM communities WHERE id > 20;
+UPDATE communities_users SET community_id = 1 + RAND() * 20, user_id = 1 + RAND() * 100; 
+SELECT * FROM communities_users;
+
+UPDATE users SET created_at = updated_at WHERE created_at > updated_at;
+
+# Создание таблиц
+
+CREATE TABLE posts (
+  id INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY, 
+  media_id INT UNSIGNED, 
+  user_id INT UNSIGNED not null,
+  head VARCHAR(255), 
+  body MEDIUMTEXT, 
+  created_at DATETIME DEFAULT CURRENT_TIMESTAMP, 
+  updated_at DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+); 
+
+UPDATE posts SET created_at = updated_at WHERE created_at > updated_at;
+
+#CREATE TABLE likes (
+#id INT UNSIGNED NOT NULL AUTO_INCREMENT  PRIMARY KEY, 
+#user_make_like_id INT UNSIGNED NOT NULL, 
+#user_get_like_id INT UNSIGNED,
+#post_id INT UNSIGNED,
+#message_id INT UNSIGNED,
+#media_id INT UNSIGNED
+#created_at DATETIME DEFAULT CURRENT_TIMESTAMP, 
+#);
+
+
+# Добавление id_user в таблицу profile, но так как у меня уже был решил сделать это для столбца id_photo
+
+#UPDATE profiles SET photo_id = NULL;
+#SELECT * FROM profiles;
+#CREATE TEMPORARY TABLE id_photo(id;
+#INSERT INTO profiles(photo_id ) SELECT media_tipe_id FROM media;
